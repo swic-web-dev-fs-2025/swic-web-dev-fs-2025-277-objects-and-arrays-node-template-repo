e834ac6fb2ec5f5af14aad6abd2a0727eb8c1c37
Manav Misra <manavm@visionwebsoft.com>
2025-09-27 09:31:44 -0500

    feat: :sparkles: add initial data structure for courses and students

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    id: "CS277",
+    name: "Web Development",
+    students: [
+      {
+        id: 1,
+        name: "Maria",
+        assignments: [
+          { name: "Project 1", points: 85, maxPoints: 100 },
+          { name: "Quiz 1", points: 18, maxPoints: 20 },
+        ],
+      },
+      {
+        id: 2,
+        name: "John",
+        assignments: [
+          { name: "Project 1", points: 92, maxPoints: 100 },
+          { name: "Quiz 1", points: 19, maxPoints: 20 },
+        ],
+      },
+    ],
+  },
+];
